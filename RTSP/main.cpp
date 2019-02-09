2dcc8bee031c7456f4d545c0a51778a4cbc8d700
M.Armoun <mohamad_armoon@yahoo.com>
2019-02-09 12:29:37 +0330

    Fix: add EVPP as network layer of application

--- RTSP/main.cpp
+++ RTSP/main.cpp
@@ -1,12 +1,48 @@
-#include "ClientSocket.h"
+#include  <iostream>
+
+#include "main.h"
+#include "evpp/tcp_conn.h"
+#include "evpp/buffer.h"
+#include "evpp/tcp_server.h"
+
+#ifdef _WIN32
+#include "winmain-inl.h"
+#endif
+
+void OnMessage(const evpp::TCPConnPtr& conn,
+	evpp::Buffer* msg) {
+	std::string s = msg->NextAllString();
+	std::cout << "Received a message "<< conn->remote_addr() <<"[" << s << "]"  << std::endl;
+	conn->Send(s);
+
+	if (s == "quit" || s == "exit") {
+		conn->Close();
+	}
+}
+
+void OnConnection(const evpp::TCPConnPtr& conn) {
+	if (conn->IsConnected()) {
+		std::cout << "Accept a new connection from " << conn->remote_addr();
+	}
+	else {
+		std::cout << "Disconnected from " << conn->remote_addr();
+	}
+}
 
 void main(int argv, char** argc)
 {
-	ClientSocket cSocket(1000,"6742","127.0.0.1");
-	ClientSocket::SocketErrorTypes response = cSocket.Connect();
-	
-	std::getchar();
-	
+	google::InitGoogleLogging(argc[0]);
+	google::SetStderrLogging(google::NUM_SEVERITIES);
+
+	std::string port = "3625";
+	std::string addr = "127.0.0.1:" + port;
+	evpp::EventLoop loop;
+	evpp::TCPServer server(&loop, addr, "RTSPClinet", 0);
+	server.SetMessageCallback(&OnMessage);
+	server.SetConnectionCallback(&OnConnection);
+	server.Init();
+	server.Start();
+	loop.Run();
 }
 
 
---
+++ RTSP/main.h
@@ -0,0 +1,7 @@
+#pragma once
+#define WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+//#pragma comment(lib, "..\\libs\\evpp_static.lib")
+#else
+
+#endif
